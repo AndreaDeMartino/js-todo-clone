e3b86dee9227b022a6c4800f2799e6350023c8f6
Andrea De Martino <ademartino1992@gmail.com>
2020-04-16 17:53:43 +0200

    Create: Fix style and add Logic Js

--- script/main.js
+++ script/main.js
@@ -1,17 +1,64 @@
 $(document).ready(function () {
-
-
-
-
-
-
-
-
-
-
-
-
   
-  
+  // General Variables
+  var list = $('.items');
+
+  // Click Button Add
+  // addButton.click(function () {
+  //   addElement();
+  // })
+
+  $('#app').on('click', '.plus-button', function(){
+    addElement();
+    
+  })
+
+  // Invio su InputBox
+  $('#app').on('keyup', '.add-item', function(e){
+    
+    if (e.which == 13){
+      console.log('Premuto Invio su Input');
+      
+      addElement();
+    }
+  })
+
+  // Funzione ADD Element
+  function addElement() {
+    // Clono struttura template li
+    var item = $('.template li').clone();
+    // Prendo valore da inputbox
+    itemValue = $('.add-item').val().trim();
+    console.log('Valore Input Box Prelevato:', itemValue);
+    if (itemValue == '') {
+      alert('Attenzione, non hai inserito un testo valido')
+    } else{
+      // Aggiungo valore inputbox a clone
+      item.append(itemValue);
+
+      // Inserisco il tutto in lista
+      list.append(item);
+
+      // Svuoto InputBox
+      $('.add-item').val('');
+    }
+  }
+
+  // Gestione Selezione item
+  $('#app').on('click','.item i', function(){
+    console.log('Click Icon');
+    var icon = $(this);
+    if (icon.hasClass('far')){
+      icon.removeClass('far').addClass('fas').css('color','#F9A35C');
+    } else{
+      icon.removeClass('fas').addClass('far').css('color','#ccc');
+    }
+  })
+
+  // Cancellazione Item
+  $('#app').on('click','.delete-button', function(){
+    console.log('delete');
+    $('.fas').parent('li').remove();
+  })
 
 });
